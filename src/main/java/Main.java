2ff10e8e782d2ce5d2ce1750d856156456388983
Phil McCarthy <phil@Phils-MacBook-Air.local>
2013-03-16 14:44:48 +0000

    Got simple embedded web and servlet server working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,52 @@
 
 import java.io.IOException;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException 
+    {
+    	// Create the web server
+        Server server = new Server(8080);
         
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        // set up servlet handler for rest calls
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
+        
+        context.setContextPath("/rest");
+        //context.setResourceBase(webDir);
+        //context.setWelcomeFiles(new String[]{ "index.html" });
+        
         ServletHolder h = new ServletHolder(new ServletContainer());
         h.setInitParameter("com.sun.jersey.config.property.packages", "resources");
         context.addServlet(h, "/*");
+        
+        // set up a resource handler to serve up static content from jar html dir
+        String webDir = server.getClass().getClassLoader().getResource("html").toExternalForm();
+        System.out.println("\n\nWeb dir = " + webDir + "\n\n");
+        
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(false);
+        resource_handler.setWelcomeFiles(new String[]{ "index.html" });
+        resource_handler.setResourceBase(webDir);
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resource_handler, context });
+        
+        server.setHandler(handlers);
+        
         try {
             server.start();
             server.join();
         } catch (Exception e) {
             e.printStackTrace();
         }
-}
+    }
 }
